cf13b1168b3361a027f28efeaa29a53578838667
kenh <1228337123hrg@gmail.com>
2018-08-09 09:08:00 +0800

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+from openpyxl import load_workbook
+import xlsxwriter
 sys.path.append("..")
 import requests
 import pandas as pd
@@ -10,6 +12,9 @@ from math import ceil
 import time
 import datetime
 now = datetime.datetime.now().strftime('%Y%m%d%H%M')
+
+
+
 
 url = "https://www.iwencai.com/stockpick/cache"
 headers = {
@@ -33,6 +38,11 @@ def get_data(query):
     save_path = base_path + ".csv"
     xlsx_path = base_path + ".xlsx"
 
+    # workbook = xlsxwriter.Workbook(xlsx_path)  # 将 筛选条件 添加到 Excel备注信息
+    # workbook.set_properties({
+    #     'comments': query})
+    # workbook.close()
+
     token, total_row = get_token.get_token(query) # 获取本次查询token值
     pages = ceil(int(total_row)/70)
     # print('token=', token)
@@ -43,15 +53,27 @@ def get_data(query):
                       "WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"showType\"\r\n\r\n[\"\",\"\",\"onTable\",\"onTable\",\"onTable\"]\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
     response = requests.request("POST", url, data=payload.encode(encoding='UTF-8'), headers=headers,timeout =5)
     result = response.json()  # 返回首页结果
+    # print(result)
     # print(type(result['title']))
     result_new = []   # 表头
     for i in result['title']:
-        j = i.replace('\r', '')
-        result_new.append(j.replace('<br>', ''))
+        if isinstance(i,str):
+            j = i.replace('\r', '').replace('<br>', '')
+            # result_new.append(j.replace('<br>', ''))
+            result_new.append(j)
+        if isinstance(i,dict):
+            (key, value), = i.items()
+            key = key.replace('\r', '').replace('<br>', '')
+            for v in value:
+                # print(key + v)
+                result_new.append(key + v)
+
 
     df1 = pd.DataFrame.from_dict(result_new).T   # 表头
     df1.to_csv(save_path,mode='a',header=False,index=False,encoding='GBK')
-    df2 = pd.DataFrame.from_dict(result['result'])  # 首页表体
+
+    body = get_body(result['result'])  # 首页表体
+    df2 = pd.DataFrame.from_dict(body)
     df2.to_csv(save_path, mode='a',header=False,index=False,encoding='GBK')
 
     if pages >= 2:
@@ -63,27 +85,69 @@ def get_data(query):
             time.sleep(3)
             response = requests.request("POST", url, data=payload.encode(encoding='UTF-8'), headers=headers,timeout =10)
             result = response.json() # 返回其他页结果
-
-            df2 = pd.DataFrame.from_dict(result['result'])  # 其他页表体
+            other_body = get_body(result['result'])
+            df2 = pd.DataFrame.from_dict(other_body)  # 其他页表体
             df2.to_csv(save_path, mode='a',header=False,index=False, encoding='GBK')
 
-    writer = pd.ExcelWriter(xlsx_path)
+    workbook = xlsxwriter.Workbook(xlsx_path)  # 将 筛选条件 添加到 Excel备注信息
+    # worksheet = workbook.add_worksheet('筛选条件')
+    workbook.set_properties({
+        'comments': query})
+    workbook.close()
+
+    writer = pd.ExcelWriter(xlsx_path,engine='openpyxl')
+    book = load_workbook(writer.path)
+    writer.book = book
     condition = ['筛选条件：',query]
     condf = pd.DataFrame(condition).T
+
     condf.to_excel(writer, '筛选条件',header=False, index=False)  # CSV文件转XLSX  sheet1  筛选条件
 
     csv_to_xlsx = pd.read_csv(save_path, encoding='GBK')
     csv_to_xlsx.to_excel(writer, sheet_name='data', index=False, freeze_panes=(1, 1))  # sheet2 data
-    writer.save()
+
+    std = book.worksheets
+    # print(std)
+    book.remove(std[0])  # 删除由xlsxwriter生成的默认sheet1
+    writer.close()
+
     print('数据导出成功！\n保存路径：', xlsx_path)
-    if os.path.exists(save_path):
-        os.remove(save_path)  # 删除源CSV文件
+    # if os.path.exists(save_path):
+    #     os.remove(save_path)  # 删除源CSV文件
 
 # while True:
 #     get_data(str(input('请输入筛选条件：')))
 #     input('继续请按Enter键')
 
+def get_body(body_result):
+    body = []  # 处理一个单元格下存在多个表格情况  获取表体
+    for solo in body_result:
+        new_list = []
+        if isinstance(solo, list):
+            for zi in solo:
+                if isinstance(zi, list):
+                    st = ''
+                    for value in zi:
+                        if isinstance(value, dict):
+                            for id in value:
+                                st = str(value[id]) + '。' + st
+                        else:
+                            # print(value)
+                            new_list.append(value)
+                            # print(st)
+                    new_list.append(st)
+                else:
+                    # print(zi)
+                    new_list.append(zi)
+        else:
+            # print(solo)
+            new_list.append(solo)
+        body.append(new_list)
+    return body
+
 
 # get_data("预测涨停板")
+get_data("近2天公告利好")
 # get_data("业绩预增")
-get_data("大单净量>1")
+# get_data("大单净量>0 筹码集中")
+# get_data("连续2日 dde大单净量大于0")
